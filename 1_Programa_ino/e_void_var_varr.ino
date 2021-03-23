9354e0cca0601f113b8475fe490da4a7f9366c75
Mariana Casement <marianacasementm@gmail.com>
2021-03-23 18:07:25 -0300

    Update e_void_var_varr.ino

--- 1_Programa_ino/e_void_var_varr.ino
+++ 1_Programa_ino/e_void_var_varr.ino
@@ -3,17 +3,17 @@ void var_varr(){
   media = 1; //força cada varredura a não fazer uma média de medidas
   num_varr = 0;
   
-  for(num_varr >= total_varr){
+  while(num_varr < total_varr){
 
     //roda uma varredura e conta ela
-    uma_var();
+    uma_varr();
     num_varr++;
 
     //inverte a direção
-    if((hor0anti1%2) == 1){ 
-      hor0anti1 = 0; 
-    }else if((hor0anti1%2) == 0) { 
-      hor0anti1 = 1;
+    if(dir == 1){ 
+      dir = 0; 
+    }else if(dir == 0) { 
+      dir = 1;
     }
 
     for(byte i=1; i<=10; i++){
