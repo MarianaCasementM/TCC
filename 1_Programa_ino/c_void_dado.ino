f30469668e4a5fa2cf322286bd55685ddf34bd96
Mariana Casement <marianacasementm@gmail.com>
2021-03-26 14:54:14 -0300

    Update c_void_dado.ino

--- 1_Programa_ino/c_void_dado.ino
+++ 1_Programa_ino/c_void_dado.ino
@@ -1,17 +1,16 @@
 //function to print data at serial port.
 
-int dado() {
+void dado() {
   
   int med = count % dist_dados; //resto da divisão: will be zero when count is a multiple of media
   float result = 0;
+  
   if(med==0){
     for(int m=1; m<=media; m++){ //tirando a média de #media valores com motor parado
       result = result + float(analogRead(A0));
     }
     Serial.println(result/media); //imprime dados de fato
-    num_dados++;
-    return;
-    
+    num_dados++;    
   }else{
     return;
   }
